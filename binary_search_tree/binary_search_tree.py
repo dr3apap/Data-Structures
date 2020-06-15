341ae9cfbcd2ea03ca00f1864c825e3ea8385e86
Adebola <dr3apap@gmail.com>
2020-06-15 11:53:05 -0700

    All assigement done

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,9 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+from collections import deque
+
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -17,45 +20,128 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value < self.value:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = BSTNode(value)
+        else:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BSTNode(value)
+            
+            
+            
+        
 
     # Return True if the tree contains the value
     # False if it does not
+    
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+        if target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+            
+        else: 
+            if not self.right:
+               return False
+            else:
+                return self.right.contains(target)
+                
+            
+         
+              
+        
 
     # Return the maximum value found in the tree
+    
     def get_max(self):
-        pass
+        if not self.right:
+            return self.value
+        return self.right.get_max()
+        
+        
 
     # Call the function `fn` on the value of each node
+    
     def for_each(self, fn):
-        pass
+        fn(self.value)
+        if self.left:
+            self.left.for_each(fn)
+        if self.right:
+            self.right.for_each(fn)
+       
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
+    
     def in_order_print(self, node):
-        pass
+       
+        if self.left:
+            self.left.in_order_print(node)
+            print(self.value)
+        if self.right:
+            self.right.in_order_print(node)
+        
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
+    
     def bft_print(self, node):
-        pass
+        nq = deque()
+        nq.append(node)
+        while len(nq) > 0:
+            current_node = nq.popleft(0)
+            print(node.value)
+            if current_node.left:
+                nq.append(current_node.left)
+            if current_node.right:
+                nq.append(current_node.right)
+        
+        
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
+    
     def dft_print(self, node):
-        pass
-
+        stack = []
+        stack.append(node)
+        while len(stack) > 0:
+            current_node = stack.pop()
+            print(node.value)
+            if current_node.left:
+                stack.append(current_node.left)
+            if current_node.right:
+                stack.append(current_node.right)
+    
+    
     # Stretch Goals -------------------------
     # Note: Research may be required
 
     # Print Pre-order recursive DFT
+    
     def pre_order_dft(self, node):
-        pass
+        if node.left:
+            node.left.Pre_order_dft(node.left)
+        if node.right:
+            node.right.pre_order_dft(node.right)
+        
 
     # Print Post-order recursive DFT
+    
     def post_order_dft(self, node):
-        pass
+        if node.left:
+            node.left.post_order_dft(node.left)
+        if node.right:
+            node.right.post_order_dft(node.right)
+            
+
+
+        
--- queue/queue.py
+++ queue/queue.py
@@ -1,28 +1,58 @@
+import sys
+sys.path.append('./singly_linked_list')
 """
-A queue is a data structure whose primary purpose is to store and
-return elements in First In First Out order. 
+A stack is a data structure whose primary purpose is to store and
+return elements in Last In First Out order. 
 
-1. Implement the Queue class using an array as the underlying storage structure.
-   Make sure the Queue tests pass.
-2. Re-implement the Queue class, this time using the linked list implementation
+1.Implement the Stack class using an array as the underlying storage structure.
+   Make sure the Stack tests pass.
+2. Re-implement the Stack class, this time using the linked list implementation
    as the underlying storage structure.
-   Make sure the Queue tests pass.
-3. What is the difference between using an array vs. a linked list when 
-   implementing a Queue?
-   
-Stretch: What if you could only use instances of your Stack class to implement the Queue?
-         What would that look like? How many Stacks would you need? Try it!
+   Make sure the Stack tests pass.
+3.What is the difference between using an array vs. a linked list when
+implementing a Stack?
 """
-class Queue:
+
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return self.size
+
+#     def push(self, value):
+
+#         self.size += 1
+#         self.storage.append(value)
+
+#     def pop(self):
+#         if self.size == 0:
+#             return
+#         else:
+#             self.size -= 1
+#             return self.storage.pop()
+
+
+from singly_linked_list import LinkedList
+
+
+class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
     
     def __len__(self):
-        pass
+        return self.size
+    
+    def push(self, value):
+        self.size += 1
+        self.storage.add_to_tail(value)
 
-    def enqueue(self, value):
-        pass
-
-    def dequeue(self):
-        pass
+    def pop(self):
+        if self.size == 0:
+            return
+        else:
+            self.size -= 1
+            return self.storage.remove_tail()
